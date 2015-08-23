cb53e879c7fca7dc44d810d54b008a8395e58616
Sungho Park <bart0724@gmail.com>
2015-08-24 01:02:34 +0900

    [추가] MyCast 앱 생성.
    - 비디오 캐스트 어플리캐이션.

---
+++ MyPlayer/app/src/main/java/com/shpark/app/mycast/WidgetFragment.java
@@ -0,0 +1,23 @@
+package com.shpark.app.mycast;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WidgetFragment extends Fragment {
+
+    public WidgetFragment() {
+        super();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_widget, container, false);
+    }
+
+}
